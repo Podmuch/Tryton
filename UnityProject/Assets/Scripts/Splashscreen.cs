8de4988635547c3ff4749003f52f954ef7c9772d
Dominik <dominikpielak@gmail.com>
2015-05-30 19:28:21 +0200

    build

---
+++ UnityProject/Assets/Scripts/Splashscreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splashscreen : MonoBehaviour {
+
+    private const float ANIMATION_TIME = 30.0f;
+    public Vector3 startPosition;
+    public Vector3 targetPosition;
+    public RectTransform SplashScreenObject;
+	// Use this for initialization
+
+    private float animationTime = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (animationTime < ANIMATION_TIME)
+        {
+            animationTime += Time.deltaTime;
+            SplashScreenObject.anchoredPosition = Vector3.Lerp(startPosition, targetPosition, animationTime / ANIMATION_TIME);
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Application.LoadLevel("mainscene");
+        }
+	}
+}
