cc99c8e2c06696757eb90d2186a787ec2fc8373c
Jarofir <jarek_dziekan@wp.pl>
2015-05-30 16:19:10 +0200

    aaa

---
+++ UnityProject/Assets/Scripts/Player2.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+	
+	public float Gravity = 0f;	//downward force
+	public float TerminalVelocity = 200f;	//max downward speed
+	public float JumpSpeed = 6000f;
+	public float MoveSpeed = 1000f;
+	
+	public Vector3 MoveVector {get; set;}
+	public float VerticalVelocity {get; set;}
+	
+	public Rigidbody2D CharacterController;
+
+	private bool isGrounded { get{ return CharacterController.velocity.y<1&&CharacterController.velocity.y>-1;}}
+	// Use this for initialization
+	void Awake () {
+		CharacterController = gameObject.GetComponent("Rigidbody2D") as Rigidbody2D;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		checkMovement();
+		HandleActionInput();
+		processMovement();
+	}
+	
+	void checkMovement(){
+		//move l/r
+		var deadZone = 0.1f;
+		VerticalVelocity = MoveVector.y;
+		MoveVector = Vector3.zero;
+		if(Input.GetAxis("Horizontal") > deadZone || Input.GetAxis("Horizontal") < -deadZone){
+			MoveVector += new Vector3(Input.GetAxis("Horizontal"),0,0);
+		}
+		//jump
+		
+	}
+	
+	void HandleActionInput(){
+		if(Input.GetButton("Jump")){
+			jump();
+		}
+	}
+	
+	void processMovement(){
+		//transform moveVector into world-space relative to character rotation
+		//MoveVector = transform.TransformDirection(MoveVector);
+		
+		//normalize moveVector if magnitude > 1
+		if(MoveVector.magnitude > 1){
+			MoveVector = Vector3.Normalize(MoveVector * Time.deltaTime);
+		}
+		
+		//multiply moveVector by moveSpeed
+		MoveVector *= MoveSpeed;
+		
+		//reapply vertical velocity to moveVector.y
+		MoveVector = new Vector3(MoveVector.x, VerticalVelocity, MoveVector.z);
+		
+		//apply gravity
+		applyGravity();
+		
+		//move character in world-space
+		CharacterController.velocity = MoveVector ;
+	}
+	
+	void applyGravity(){
+		if(MoveVector.y > -TerminalVelocity){
+			MoveVector = new Vector3(MoveVector.x, (MoveVector.y - Gravity * Time.deltaTime), MoveVector.z);
+		}
+		if(isGrounded && MoveVector.y < -1){
+			MoveVector = new Vector3(MoveVector.x, (-1), MoveVector.z);
+		}
+	}
+	
+	public void jump(){
+		if(isGrounded){
+			VerticalVelocity = JumpSpeed;
+		}
+	}
+}
