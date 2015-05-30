2c77fb7f3d44828423d2c0078a5b8cc4385d0ce2
Koper <hwt365@gmail.com>
2015-05-30 13:45:10 +0200

    Jedzenie

---
+++ UnityProject/Assets/Scripts/FoodGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodGenerator: MonoBehaviour {
+	#region CLASS SETTINGS
+	
+	private float RESPAWN_TIME = 0.5f;
+	
+	#endregion
+	
+	#region RESOURCE REFERENCES
+	
+	public Food[] FoodPrefabs;
+	
+	#endregion
+	
+	private bool isWorking = false;
+	private float timer = 0;
+	
+	private void Update()
+	{
+		if(isWorking)
+		{
+			timer += Time.deltaTime;
+			if(timer>=RESPAWN_TIME)
+			{
+				timer = 0;
+				GameObject newFood = (GameObject)Instantiate(FoodPrefabs[UnityEngine.Random.Range(0, FoodPrefabs.Length)].gameObject);
+				newFood.transform.parent = transform;
+				newFood.transform.localScale = Vector3.one;
+				Food foodComponent = newFood.GetComponent<Food>();
+				foodComponent.Init();
+			}
+		}
+	}
+	
+	public void StartGenerator()
+	{
+		isWorking = true;
+		timer = 0;
+	}
+	
+	public void StopGenerator()
+	{
+		isWorking = false;
+	}
+}
